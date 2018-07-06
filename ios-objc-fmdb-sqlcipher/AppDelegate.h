fb377a1973d7d41bd97ea71d967c17d4bfd171a5
OkuderaYuki <>
2018-07-07 01:53:29 +0900

    first commit.

---
+++ ios-objc-fmdb-sqlcipher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-objc-fmdb-sqlcipher
+//
+//  Created by YukiOkudera on 2018/07/07.
+//  Copyright © 2018年 YukiOkudera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-objc-fmdb-sqlcipher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-objc-fmdb-sqlcipher
+//
+//  Created by YukiOkudera on 2018/07/07.
+//  Copyright © 2018年 YukiOkudera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
